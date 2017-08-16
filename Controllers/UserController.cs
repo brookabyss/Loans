031c2f346b74885ada432c450775dbc759e62a4e
Brook Kebede <brookabyss@gmail.com>
2017-08-16 16:41:00 -0700

    Lenders and Borrowers can interact , Login page working

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -50,7 +50,7 @@ namespace Loans.Controllers
                 .OrderByDescending(a=>a.CreatedAt).
                 SingleOrDefault(b=>b.Email==LenView.Email);
                 ViewBag.Lender= lender;
-
+                HttpContext.Session.SetInt32("UserId",lender.LendersId);
                  List<UserLoans> userLoans = _context.UserLoans
                                             .Include(a=>a.Borrowers)
                                             .Include(a=>a.Lenders)
@@ -90,6 +90,7 @@ namespace Loans.Controllers
                 Borrowers borrower=_context.Borrowers
                 .OrderByDescending(a=>a.CreatedAt).
                 SingleOrDefault(b=>b.Email==BorrView.Email);
+                HttpContext.Session.SetInt32("UserId",borrower.BorrowersId);
                 ViewBag.borrower=borrower;
 
                  List<UserLoans> userLoans = _context.UserLoans
@@ -105,5 +106,172 @@ namespace Loans.Controllers
                 return RedirectToAction("Index");
             }
         }
+        // Show Lender
+
+        [HttpGet]
+        [Route("lender/show")]
+        public IActionResult ShowLender()
+        {
+            ViewBag.errors= new List<string>();
+            int? Id=HttpContext.Session.GetInt32("UserId");
+            Lenders lender=_context.Lenders
+                .OrderByDescending(a=>a.CreatedAt).
+                SingleOrDefault(b=>b.LendersId==(int)Id);
+            ViewBag.Lender= lender;
+            List<UserLoans> userLoans= new List<UserLoans>();
+            userLoans = _context.UserLoans
+                                .Include(a=>a.Borrowers)
+                                .Include(a=>a.Lenders)
+                                .Where(a=>a.LendersId==lender.LendersId)
+                                .ToList();
+            List<Borrowers> borrowers= _context.Borrowers.ToList();
+            ViewBag.Borrowers= borrowers;
+            ViewBag.UserLoans= userLoans;
+            if(TempData["Errors"]!=null){
+                ViewBag.errors.Add(TempData["Errors"]);
+            }
+            
+            return View("lender");
+
+        }
+
+
+        // Show Borrower
+
+        [HttpGet]
+        [Route("Borrower/show")]
+        public IActionResult ShowBorrower()
+        {
+            ViewBag.errors= new List<string>();
+            int? Id=HttpContext.Session.GetInt32("UserId");
+            Borrowers borrower=_context.Borrowers
+                .OrderByDescending(a=>a.CreatedAt).
+                SingleOrDefault(b=>b.BorrowersId==(int)Id);
+            ViewBag.Borrower= borrower;
+            List<UserLoans> userLoans = _context.UserLoans
+                                            .Include(a=>a.Borrowers)
+                                            .Include(a=>a.Lenders)
+                                            .Where(a=>a.BorrowersId==borrower.BorrowersId)
+                                            .ToList();
+            ViewBag.UserLoans= userLoans;
+            ViewBag.UserLoans= userLoans;
+            if(TempData["Errors"]!=null){
+                ViewBag.errors.Add(TempData["Errors"]);
+            }
+            
+            return View("borrower");
+
+        }
+
+
+
+
+        [HttpPost]
+        [Route("lend/{BId}")]
+        public IActionResult Lend(int BId, int Money)
+        {
+            
+            int? Id=HttpContext.Session.GetInt32("UserId");
+            
+            Lenders lender=_context.Lenders
+                .OrderByDescending(a=>a.CreatedAt).
+                SingleOrDefault(b=>b.LendersId==(int)Id);
+        
+            if (lender.Bank < Money){
+                TempData["Errors"]="Insufficient Funds!";
+                
+            }
+            else{
+
+                Borrowers borrower=_context.Borrowers
+                .OrderByDescending(a=>a.CreatedAt).
+                SingleOrDefault(b=>b.BorrowersId==BId);
+                
+                UserLoans checkLoan= _context.UserLoans
+                                            .Include(a=>a.Borrowers)
+                                            .Include(a=>a.Lenders)
+                                            .SingleOrDefault(a=>(a.BorrowersId==BId) && (a.LendersId==(int)Id));
+                if(checkLoan!=null){
+                    checkLoan.Amount=checkLoan.Amount+Money;
+                }
+                else{
+
+                    UserLoans newLoan= new UserLoans{
+                    Amount= Money,
+                    BorrowersId=BId,
+                    LendersId=(int)Id,
+                    CreatedAt=DateTime.Now,
+                    UpdatedAt= DateTime.Now
+                    };
+                     _context.UserLoans.Add(newLoan);
+
+                }
+
+                borrower.Raised=borrower.Raised+Money;
+                lender.Lent=lender.Lent+Money;
+                lender.Bank=lender.Bank-Money;
+                _context.SaveChanges();
+
+
+            }
+
+            return RedirectToAction("ShowLender");
+          
+            
+        }
+        // To Login 
+
+        [HttpGet]
+        [Route("user/login")]
+        public IActionResult ToLogin(){
+            ViewBag.errors=new List<string>();
+            return View("login");
+        }
+
+        [HttpPost]
+        [Route("member/Login")]
+        public IActionResult Login(string Email, string Password){
+            Borrowers borrower= new Borrowers();
+            Lenders lender = new Lenders();
+             borrower= _context.Borrowers.SingleOrDefault(a=>a.Email==Email);
+           lender= _context.Lenders.SingleOrDefault(a=>a.Email==Email);
+
+            if(borrower==null&& lender==null){
+                ViewBag.errors.Add("Email doesn't exist please register");
+                return View("Index");
+            }
+            else{    
+                    if (borrower!=null){
+                         if (borrower.Password==Password){
+                            HttpContext.Session.SetInt32("UserId",borrower.BorrowersId);
+                            return RedirectToAction("ShowBorrower");
+                        }
+                    }  
+                    else if (lender!=null){
+                        if(lender.Password==Password){
+                            HttpContext.Session.SetInt32("UserId",lender.LendersId);
+                            return RedirectToAction("ShowLender");
+                        }
+                    }
+                     
+                    else{
+                        ViewBag.errors.Add("Email or password is incorrect");
+                    }
+                   
+                    return View("Index");
+                }
+        
+
+        }
+
+        [HttpGet]
+        [Route("user/logout")]
+        public IActionResult Logout(){
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+
+
     }
 }
